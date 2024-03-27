1cbe71038c60dc518fd9c5cf0d527f638d942acc
yjyolandeyan <158221697+yjyolandeyan@users.noreply.github.com>
2024-03-26 23:19:37 -0400

    Update popup.js

--- tabs-manager/popup.js
+++ tabs-manager/popup.js
@@ -34,6 +34,6 @@ button.addEventListener('click', async () => {
   const tabIds = tabs.map(({ id }) => id);
   if (tabIds.length) {
     const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: 'DOCS' });
+    await chrome.tabGroups.update(group, { title: 'DevDocs' });
   }
 });
